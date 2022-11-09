f169986a2d0daa156092c7b80a1d42f51ae5e159
Doro256 <dorothynantumbwe@yahoo.com>
2022-11-09 20:18:32 +0000

    Edit

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,6 +2,6 @@
 exec {'/usr/bin/env apt-get update':}
 exec {'/usr/bin/env apt-get -y install nginx':}
 exec {'/usr/bin/env ufw allow "Nginx HTTP"':}
-exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html':}
+exec {'/usr/bin/env echo "Hello World!" > /var/www/html/index.nginx-debian.html':}
 exec {'/usr/bin/env sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default':}
 exec {'/usr/bin/env service nginx start':}
